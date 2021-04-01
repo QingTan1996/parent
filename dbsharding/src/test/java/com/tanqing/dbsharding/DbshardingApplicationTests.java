5595f49e5d024f1ff21ad1cda756320ac2e213f6
tanqing <qingtandlut@outlook.com>
2021-04-01 10:49:31 +0800

    添加dbsharding

---
+++ dbsharding/src/test/java/com/tanqing/dbsharding/DbshardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanqing.dbsharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbshardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
